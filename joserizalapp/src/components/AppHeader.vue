ab1e4804d58e0b4b4de53db316120e0c68938de9
LynnDelaere <lynn.delaere@hotmail.be>
2025-03-03 16:05:49 +0100

    Started making header component

---
+++ joserizalapp/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navBar">
+        <v-app-bar>
+            
+        </v-app-bar>
+
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
--- joserizalapp/src/views/HomePage.vue
+++ joserizalapp/src/views/HomePage.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
+        <AppHeader />
         <h1>Home Page</h1>
     </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue';
+
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    components: {
+        AppHeader,
+    }
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
