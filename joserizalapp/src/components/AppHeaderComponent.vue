aeb29a18d0a3c16305730e70b2cbaf281bc230a0
LynnDelaere <lynn.delaere@hotmail.be>
2025-03-13 17:06:21 +0100

    Started building a menu bar

--- joserizalapp/src/components/AppHeaderComponent.vue
+++ joserizalapp/src/components/AppHeaderComponent.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="appHeader">
         <h1>{{ title }}</h1>
-        <button><img src="../assets/list.svg" /></button>
+        <button id="menu" @click="toggleMenu"><img src="../assets/list.svg" /></button>
+        
+        <!-- Menu bar that shows/hides based on isMenuOpen -->
+        <div class="menu-bar" v-if="isMenuOpen">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -14,6 +24,17 @@ export default {
             required: true,
             default: 'Default Title'
         }
+    },
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+            console.log('Menu clicked, isMenuOpen:', this.isMenuOpen);
+        }
     }
 }
 </script>
@@ -24,11 +45,45 @@ export default {
     align-items: center;
     justify-content: space-between;
     padding: 1rem;
+    position: relative;
 }
 
 .appHeader button {
     background: none;
     border: none;
     cursor: pointer;
+}
+
+.menu-bar {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    background-color: white;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    width: 200px;
+    z-index: 10;
+}
+
+.menu-bar ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.menu-bar li {
+    padding: 0;
+}
+
+.menu-bar a {
+    display: block;
+    padding: 10px 15px;
+    text-decoration: none;
+    color: #333;
+    transition: background-color 0.3s;
+}
+
+.menu-bar a:hover {
+    background-color: #f5f5f5;
 }
 </style>
